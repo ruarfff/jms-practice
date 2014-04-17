b686285b8003b4ff2d794a50a4875546caaa0e81
tussle <gaming124@gmail.com>
2014-04-17 10:59:05 +0100

    A start

--- jms-stuff-thomas/src/main/java/jms/receiver/AsyncReceiver.java
+++ jms-stuff-thomas/src/main/java/jms/receiver/AsyncReceiver.java
@@ -1,5 +1,51 @@
 package jms.receiver;
+import javax.jms.MessageListener;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+public class AsyncReceiver implements MessageListener, MessageConsumer {
 
-public class AsyncReceiver {
-
+	public AsyncReceiver(){
+		
+	}
+	@Override
+	public void onMessage(Message message){
+		
+	}
+	
+	
+	
+	@Override
+	public MessageListener getMessageListener(){
+		return null;
+	}
+	
+	 /* start of Unimplemented block */
+	@Override
+	public void close(){
+		
+	}
+	@Override
+	public String getMessageSelector(){
+		return null;
+	}
+	
+	@Override
+	public Message receive(){
+		return null;
+	}
+	
+	@Override
+	public Message receive(long timeout){
+		
+		return null;
+	}
+	
+	public Message receiveNoWait(){
+		return null;
+	}
+	/*end of unimplemented block*/
+	@Override
+	public void setMessageListener(MessageListener listener){
+		
+	}
 }
