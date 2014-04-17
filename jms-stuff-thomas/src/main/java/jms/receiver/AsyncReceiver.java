af6921ce194fce5991d21c96d826fcff99a9f2ae
tussle <gaming124@gmail.com>
2014-04-17 14:18:57 +0100

    a basic sender receiver
    messages produced to toms test queue on the groups activemq server and
    retrieved from the same queue, just once each time

--- jms-stuff-thomas/src/main/java/jms/receiver/AsyncReceiver.java
+++ jms-stuff-thomas/src/main/java/jms/receiver/AsyncReceiver.java
@@ -2,50 +2,22 @@ package jms.receiver;
 import javax.jms.MessageListener;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
-public class AsyncReceiver implements MessageListener, MessageConsumer {
+import javax.jms.TextMessage;
+public class AsyncReceiver implements MessageListener {
 
 	public AsyncReceiver(){
 		
 	}
 	@Override
 	public void onMessage(Message message){
-		
+		if(message instanceof TextMessage){
+			TextMessage tm = (TextMessage) message;
+			try{
+				System.out.println("this is the response "+tm.getText());
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
 	}
 	
-	
-	
-	@Override
-	public MessageListener getMessageListener(){
-		return null;
-	}
-	
-	 /* start of Unimplemented block */
-	@Override
-	public void close(){
-		
-	}
-	@Override
-	public String getMessageSelector(){
-		return null;
-	}
-	
-	@Override
-	public Message receive(){
-		return null;
-	}
-	
-	@Override
-	public Message receive(long timeout){
-		
-		return null;
-	}
-	
-	public Message receiveNoWait(){
-		return null;
-	}
-	/*end of unimplemented block*/
-	@Override
-	public void setMessageListener(MessageListener listener){
-		
-	}
 }
